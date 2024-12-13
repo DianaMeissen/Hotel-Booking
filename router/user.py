04f3265887578f6e538a4db89067b90e3a836428
Diana Meissen <vishnya054@gmail.com>
2024-12-13 16:56:06 +0100

    updated gitignore and base user functionality

--- router/user.py
+++ router/user.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from schemas import UserBase, UserDisplay
 from db.database import get_db
+from schemas import UserBase, UserDisplay
+
 from db import db_user
 
 router = APIRouter(
@@ -9,6 +10,15 @@ router = APIRouter(
     tags=["user"]
 )
 
+# Crearte user
 @router.post("/", response_model=UserDisplay)
 def create_user(request:UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
+
+# Read all users
+
+# Read one user
+
+# Update user
+
+# Delete user
