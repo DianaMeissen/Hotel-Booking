f73cd4bd4785122e6b674e81a45f325a13a41600
Diana Meissen <vishnya054@gmail.com>
2024-12-16 21:25:49 +0100

    added token check

--- router/hotel.py
+++ router/hotel.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import get_db
-from schemas import HotelBase, HotelDisplay
-from auth.oauth2 import oauth2_scheme
+from schemas import UserBase, HotelBase
+from auth.oauth2 import get_current_user, oauth2_scheme
 
 from db import db_hotel
 
@@ -16,6 +16,9 @@ router = APIRouter(
 def create_hotel(request: HotelBase, db: Session = Depends(get_db)):
     return db_hotel.create_hotel(db, request)
 
-@router.get('/{id}', response_model=HotelDisplay)
-def get_hotel(id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-    return db_hotel.get_hotel(db, id)
+@router.get('/{id}')
+def get_hotel(id: int, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
+    return {
+        'data': db_hotel.get_hotel(db, id),
+        'current_user': current_user
+    }
