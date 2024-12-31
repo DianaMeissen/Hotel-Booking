58877cf6cc24e35418fb1bca90116e3ceefab22c
Diana Meissen <vishnya054@gmail.com>
2024-12-31 14:31:51 +0100

    added middleware

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from router import user, hotel, payment, room, payment, booking
 from db import models
 from db.database import engine
 from router import authentification
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(authentification.router)
@@ -17,3 +18,13 @@ def index():
     return {"message": "Welcome to the booking service"}
 
 models.Base.metadata.create_all(engine)
+
+origins = ['http://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins, 
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
